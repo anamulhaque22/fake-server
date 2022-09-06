6975fe1089e1d2270da15bf94f7c1ac0df0f9fb5
anamulhaque22 <anamulah535367@gmail.com>
2022-09-06 17:17:58 +0600

    transaction data

---
+++ db.js
@@ -0,0 +1,7 @@
+const videos = require("./data/videos");
+const transactions = require("./data/transactions");
+
+module.exports = {
+    videos,
+    transactions,
+}
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const data = require("./db_videos.json");
+const data = require("./db.js");
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 9000;
 
 server.use(middlewares);
 server.use(router);
