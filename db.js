d4af4627b00b1cc704bfc0787a80bd166cf13896
anamulhaque22 <anamulah535367@gmail.com>
2022-09-08 17:23:22 +0600

    todos

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const videos = require("./data/videos.json");
 const transactions = require("./data/transactions.json");
-const tags = require('./data/tags.json')
+const tags = require('./data/tags.json');
+const todos = require('./data/todos.json');
 
 module.exports = {
     videos,
     transactions,
     tags,
+    todos,
 }
